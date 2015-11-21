83947845517a05edbeda30e69e6090ec150115d8
Antonio Tenorio Fornés <antoniotenorio@ucm.es>
2015-11-21 19:32:34 +0100

    pull menu items to the left

--- templates/header.php
+++ templates/header.php
@@ -1,6 +1,6 @@
 <header class="banner navbar navbar-default navbar-fixed-top" role="banner">
   <div class="container">
-    <nav class="collapse navbar-collapse" role="navigation">
+    <nav class="collapse navbar-collapse pull-right" role="navigation">
       <?php
       if (has_nav_menu('primary_navigation')) :
         wp_nav_menu(['theme_location' => 'primary_navigation', 'menu_class' => 'nav navbar-nav']);
