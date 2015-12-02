31354881b99422e2055698e74c40a630edff6be9
Antonio Tenorio Fornés <antoniotenorio@ucm.es>
2015-12-02 21:00:00 +0100

    added navigation menu for small screens

--- templates/header.php
+++ templates/header.php
@@ -1,5 +1,13 @@
 <header class="banner navbar navbar-default navbar-fixed-top" role="banner">
   <div class="container">
+    <div class="navbar-header">
+      <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target=".navbar-collapse">
+        <span class="sr-only"><?= __('Toggle navigation', 'sage'); ?></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+      </button>
+    </div>
     <nav class="collapse navbar-collapse pull-right" role="navigation">
       <?php
       if (has_nav_menu('primary_navigation')) :
