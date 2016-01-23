e19cf507ee768a076bc076dae934f357b6636fa3
Antonio Tenorio Fornés <antoniotenorio@ucm.es>
2016-01-23 21:56:06 +0100

    disable sidebar

--- lib/setup.php
+++ lib/setup.php
@@ -94,6 +94,7 @@ function display_sidebar() {
     is_404(),
     is_front_page(),
     is_page_template('template-custom.php'),
+    true
   ]);
 
   return apply_filters('sage/display_sidebar', $display);
